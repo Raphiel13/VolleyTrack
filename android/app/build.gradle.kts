9f61bdc8f973e44fb3d886f781bbbda3632c243e
Rafal <rafal.czaja132@gmail.com>
2026-05-05 14:33:07 +0200

    [chore] changed app icon from default to custom

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.rafal.volleymanagerapp"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 16
-        versionName = "0.1.2"
+        versionCode = 17
+        versionName = "0.1.3"
         manifestPlaceholders["MAPS_API_KEY"] = 
             localProperties.getProperty("MAPS_API_KEY", "")
     }
